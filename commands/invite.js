8fb6644c894d3faf62a9491afa31ab5b15adb75a
mmadrid509 <63370132+mmadrid509@users.noreply.github.com>
2020-05-14 20:00:36 -0700

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'invite',
+	description: 'Inlcudes info on SCP-███',
+	execute(message, args) {
+		message.channel.send('Here is the official invite for Foundation Information! https://discord.com/oauth2/authorize?client_id=702659948030459927&permissions=120832&scope=bot');
+	},
+};
