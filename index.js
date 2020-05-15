5d6eef4aab02d4f3682bd34d234ccacb2c066413
mmadrid509 <63370132+mmadrid509@users.noreply.github.com>
2020-05-14 18:46:45 -0700

    Updating for currency

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const { prefix, token } = require('./config.json');
+const {prefix} = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const config = require("./config.json")
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const { Users, CurrencyShop } = require('./currency-stuff/dbObjects');
+const { Op } = require('sequelize');
+const currency = new Discord.Collection();
+const PREFIX = ';';
+const app = fs.readdirSync('./currency').filter(file => file.endsWith('.js'));
+
+for (const file of app) {
+	const currency = require(`./currency/app.js`);
+	
+	
+	client.commands.set(currency.name, currency);
+}
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
+	
+	
 	client.commands.set(command.name, command);
 }
 
 
 const cooldowns = new Discord.Collection();
-client.once('ready', () => {
+
+client.once('ready', async () => {
+	const storedBalances = await Users.findAll();
+storedBalances.forEach(b => currency.set(b.user_id, b));
 	console.log('Sucessfuly Opened SCP Foundation Files!');
 });
 
 
-client.on('message', message => {
+client.on('message', async message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 const args = message.content.slice(prefix.length).split(/ +/);
@@ -29,11 +43,12 @@ if (!client.commands.has(commandName)) return;
 const command = client.commands.get(commandName);
 
 
-try {
+	
+try  {
 	command.execute(message, args);
 } catch (error) {
 	console.error(error);
-	message.reply('there was an error trying to execute that command!');
+	message.reply('there was an error trying to execute that command! Check the terminal for more info.');
 }
 });
 
