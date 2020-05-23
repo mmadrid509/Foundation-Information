35d136dc4a644154665d87dcb1e6c0cf141b7be7
mmadrid509 <63370132+mmadrid509@users.noreply.github.com>
2020-05-22 17:31:28 -0700

    Prepared for embed.

--- commands/scp-002.js
+++ commands/scp-002.js
@@ -1,3 +1,15 @@
+const Discord = require('discord.js')
+const SCP002 = new Discord.MessageEmbed()
+	.setColor('#010000')
+	.setTitle('SCP-002')
+	.setURL('http://www.scp-wiki.net/scp-002')
+	.setAuthor('SCP Foundation')
+	.setDescription('GENERAL NOTICE 001-Alpha: In order to prevent knowledge of SCP-001 from being leaked, several/no false SCP-001 files have been created alongside the true file/files. All files concerning the nature of SCP-001, including the decoy/decoys, are protected by a memetic kill agent designed to immediately cause cardiac arrest in any nonauthorized personnel attempting to access the file. Revealing the true nature/natures of SCP-001 to the general public is cause for execution, except as required under ████-███-██████.')
+	.setThumbnail('https://cdn.discordapp.com/attachments/692738250514890773/713418940734505010/download_7.jpg')
+
+
+	
+	.setTimestamp()
 module.exports = {
 	name: '002',
     cooldown: 5,
