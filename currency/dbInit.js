dde19b5b4a122e98634cda42fadb8f7cd6082c09
mmadrid509 <63370132+mmadrid509@users.noreply.github.com>
2020-06-01 17:20:26 -0700

    New Items

--- currency/dbInit.js
+++ currency/dbInit.js
@@ -15,9 +15,12 @@ const force = process.argv.includes('--force') || process.argv.includes('-f');
 
 sequelize.sync({ force }).then(async () => {
 	const shop = [
-		CurrencyShop.upsert({ name: 'Foundation Key', cost: 5 }),
-		CurrencyShop.upsert({ name: 'Coffee From SCP-294', cost: 2 }),
-		CurrencyShop.upsert({ name: '05 Council Keycard', cost: 100 }),
+		CurrencyShop.upsert({ name: 'Foundation Key', cost: 25, type: 'Collectible' }),
+		CurrencyShop.upsert({ name: 'Coffee From SCP-294', cost: 50, type: 'Collectible'}),
+		CurrencyShop.upsert({ name: '05 Council Keycard', cost: 100, type: 'Collectible'}),
+		CurrencyShop.upsert({ name: `Mmadrid509's Head`, cost: 150, type: 'Collectible'}),
+		CurrencyShop.upsert({ name: `McDonald's Fries`, cost: 690, type: 'Collectible'}),
+		CurrencyShop.upsert({ name: 'InfiCrate', cost: 10000, type: 'Crate'}),
 	];
 	await Promise.all(shop);
 	console.log('Database synced');
